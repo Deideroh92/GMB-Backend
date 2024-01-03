694e8d5bfcc7c46b69594468d8d1e91c6ced8ce2
Maximilien DAVID <maximiliend1998@hotmail.fr>
2024-01-03 22:15:49 +0100

    solving errors

--- GMB.Tests/Launch.cs
+++ GMB.Tests/Launch.cs
@@ -30,12 +30,10 @@ namespace GMB.Tests
         [TestMethod]
         public void ThreadsUrlScraper()
         {
-            string[] categories = File.ReadAllLines(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "GMB.Sdk.Core\\Files", "Categories.txt"));
-            string[] textSearch = File.ReadAllLines(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "GMB.Sdk.Core\\Files", "UrlTextSearch.txt"));
-            string[] dept = File.ReadAllLines(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "GMB.Sdk.Core\\Files", "DeptList.txt"));
-            string[] idf = File.ReadAllLines(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "GMB.Sdk.Core\\Files", "IleDeFrance.txt"));
-            string[] cp = File.ReadAllLines(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "GMB.Sdk.Core\\Files", "CpList.txt"));
-            string[] customLocations = File.ReadAllLines(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "GMB.Sdk.Core\\Files", "CustomLocations.txt"));
+            string[] categories = File.ReadAllLines(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "Files", "Categories.txt"));
+            string[] dept = File.ReadAllLines(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "Files", "DeptList.txt"));
+            string[] idf = File.ReadAllLines(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "Files", "IleDeFrance.txt"));
+            string[] cp = File.ReadAllLines(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "Files", "CpList.txt"));
 
             List<string> locations = new(cp);
             List<Task> tasks = [];
@@ -83,7 +81,7 @@ namespace GMB.Tests
         [TestMethod]
         public void SetProcessingFromIdEtabFile()
         {
-            string filePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName + @"\Files\Custom.txt";
+            string filePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName + @"GMB.Sdk.Core\Files\Custom.txt";
             using DbLib db = new();
 
             List<string> values = [];
@@ -104,70 +102,15 @@ namespace GMB.Tests
             }
         }
         /// <summary>
-        /// Exporting Hotels info.
-        /// </summary>
-        [TestMethod]
-        public void GetIdEtabByPlaceId()
-        {
-            string[] placeIds = File.ReadAllLines(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "GMB.Sdk.Core\\Files", "urls.txt"));
-            using DbLib db = new();
-            DbBusinessProfile? bp = null;
-            using StreamWriter sw2 = File.AppendText(@"C:\Users\maxim\Desktop\test.txt");
-
-            foreach (var id in placeIds)
-            {
-                bp = db.GetBusinessByPlaceId(id);
-                if (bp != null)
-                    sw2.WriteLine(bp.IdEtab);
-                else
-                    sw2.WriteLine(id);
-            }
-        }
-        /// <summary>
-        /// Exporting Hotels info.
-        /// </summary>
-        [TestMethod]
-        public async Task ExportHotelAsync()
-        {
-
-            // CONFIG
-            int nbEntries = 1;
-            UrlState urlState = UrlState.NEW;
-
-            using DbLib db = new();
-            List<BusinessAgent> businessList = db.GetBusinessAgentListByUrlState(urlState, nbEntries);
-            ScannerFunctions toolbox = new();
-
-            using SeleniumDriver driver = new();
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("fr-FR");
-            using StreamWriter sw2 = File.AppendText(@"C:\Users\maxim\Desktop\hotel.txt");
-            sw2.WriteLine("NAME$$CATEGORY$$ADRESS$$TEL$$OPTIONS");
-            foreach (BusinessAgent elem in businessList)
-            {
-                try
-                {
-                    GetBusinessProfileRequest request = new(elem.Url, null, null);
-                    (DbBusinessProfile? business, DbBusinessScore? businessScore) = await toolbox.GetBusinessProfileAndScoreFromGooglePageAsync(driver, request, null);
-                    var optionsOn = ToolBox.FindElementsSafe(driver.WebDriver, XPathProfile.optionsOn);
-                    List<string> optionsOnList = [];
-                    foreach (IWebElement element in optionsOn)
-                    {
-                        optionsOnList.Add(element.GetAttribute("aria-label").Replace("L'option ", "").Replace(" est disponible", ""));
-                    }
-                    sw2.WriteLine(business.Name + "$$" + business.Category + "$$" + business.GoogleAddress + "$$" + business.Tel + "$$" + string.Join(",", optionsOnList));
-                } catch (Exception) { }
-            }
-        }
-        /// <summary>
         /// Starting Scanner.
         /// </summary>
         [TestMethod]
-        public async void LaunchBusinessScanner()
+        public void LaunchBusinessScanner()
         {
             ScannerController controller = new();
-            BusinessScannerRequest request = new(100000, 1, Operation.PROCESSING_STATE, true, DateTime.UtcNow.AddMonths(2), true);
+            BusinessScannerRequest request = new(100000, 1, Operation.PROCESSING_STATE, true, DateTime.UtcNow.AddDays(15), true);
 
-            await controller.StartBusinessScannerAsync(request);
+            _ = controller.StartBusinessScannerAsync(request);
             return;
         }
         #endregion
